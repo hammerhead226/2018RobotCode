780d261f631e06f6191757bc97604290fef06848
njaison <nidhijaison@gmail.com>
2018-02-10 12:41:19 -0500

    Changed Voltage
    💯
    In Christ, Steve Wattanabe aka Varun Rajesh
    
    Co-Authored-By: vrrajesh21 <vrrajesh21@users.noreply.github.com>

--- src/org/usfirst/frc/team226/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team226/robot/subsystems/Elevator.java
@@ -13,71 +13,71 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 
-//aUTh0R = vAruN rAj35h
+// aUTh0R = vAruN rAj35h
 public class Elevator extends Subsystem {
-	
+
 	TalonSRX frontLeft = new TalonSRX(RobotMap.EL_FRONT_LEFT);
-    TalonSRX frontRight = new TalonSRX(RobotMap.EL_FRONT_RIGHT);
-    TalonSRX rearLeft = new TalonSRX(RobotMap.EL_REAR_LEFT);
-    TalonSRX rearRight = new TalonSRX(RobotMap.EL_REAR_RIGHT);
-    
-    DigitalInput hallEffect = new DigitalInput(RobotMap.HALL_EFFECT_SENSOR);
-    
-    double intakeHeight;
-    double switchHeight;
-    double powHeight;
-    double scaleHeight;
-    
+	TalonSRX frontRight = new TalonSRX(RobotMap.EL_FRONT_RIGHT);
+	TalonSRX rearLeft = new TalonSRX(RobotMap.EL_REAR_LEFT);
+	TalonSRX rearRight = new TalonSRX(RobotMap.EL_REAR_RIGHT);
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	DigitalInput hallEffect = new DigitalInput(RobotMap.HALL_EFFECT_SENSOR);
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-    
-    public Elevator() {
-    	frontLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-    	
-    	frontLeft.configVoltageCompSaturation(11.5, 0);
-    	frontRight.configVoltageCompSaturation(11.5, 0);
-    	rearLeft.configVoltageCompSaturation(11.5, 0);
-    	rearRight.configVoltageCompSaturation(11.5, 0);
-    	
-    	frontLeft.enableVoltageCompensation(true);
-    	frontRight.enableVoltageCompensation(true);
-    	rearLeft.enableVoltageCompensation(true);
-    	rearRight.enableVoltageCompensation(true);
-    	
-    	rearLeft.setInverted(true);
-    	rearRight.setInverted(true);
-   
-    	frontRight.follow(frontLeft);
-        rearLeft.follow(frontLeft);
-        rearRight.follow(frontLeft);
-    }
-    public void setElevator(int height) {
-    	switch(height) {
-    	case 0:
-    		frontLeft.set(ControlMode.MotionMagic, intakeHeight);
-    		break;
-    	case 1:
-    		frontLeft.set(ControlMode.MotionMagic, switchHeight);
-    		break;
-    	case 2:
-    		frontLeft.set(ControlMode.MotionMagic, powHeight);
-    		break;
-    	case 3:
-    		frontLeft.set(ControlMode.MotionMagic, scaleHeight);
-    		break;
-    	}
-    	
-    }
-    public void zero() {
-    	if(hallEffect.get()) {
-    		frontLeft.setSelectedSensorPosition(0, 0, 0);
-    	}
-    }
+	double intakeHeight;
+	double switchHeight;
+	double powHeight;
+	double scaleHeight;
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+
+	public Elevator() {
+		frontLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+
+		frontLeft.configVoltageCompSaturation(11, 0);
+		frontRight.configVoltageCompSaturation(11, 0);
+		rearLeft.configVoltageCompSaturation(11, 0);
+		rearRight.configVoltageCompSaturation(11, 0);
+
+		frontLeft.enableVoltageCompensation(true);
+		frontRight.enableVoltageCompensation(true);
+		rearLeft.enableVoltageCompensation(true);
+		rearRight.enableVoltageCompensation(true);
+
+		rearLeft.setInverted(true);
+		rearRight.setInverted(true);
+
+		frontRight.follow(frontLeft);
+		rearLeft.follow(frontLeft);
+		rearRight.follow(frontLeft);
+	}
+
+	public void setElevator(int height) {
+		switch (height) {
+		case 0:
+			frontLeft.set(ControlMode.MotionMagic, intakeHeight);
+			break;
+		case 1:
+			frontLeft.set(ControlMode.MotionMagic, switchHeight);
+			break;
+		case 2:
+			frontLeft.set(ControlMode.MotionMagic, powHeight);
+			break;
+		case 3:
+			frontLeft.set(ControlMode.MotionMagic, scaleHeight);
+			break;
+		}
+
+	}
+
+	public void zero() {
+		if (hallEffect.get()) {
+			frontLeft.setSelectedSensorPosition(0, 0, 0);
+		}
+	}
 }
-
