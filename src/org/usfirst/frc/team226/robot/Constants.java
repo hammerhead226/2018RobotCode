fe59650ea404cc2cac508b61aca5fc4bd964376b
Alec Minchington <minchingtonak@users.noreply.github.com>
2018-02-10 16:05:05 -0500

    Drivetrain talons configured on startup, add Constants

---
+++ src/org/usfirst/frc/team226/robot/Constants.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team226.robot;
+
+public class Constants {
+	
+	public static final int STARTUP_WAIT = 10;
+
+	public static final double DT_VOLTAGE_LIMIT = 11;
+	public static final int DT_CURRENT_LIMIT = 30;
+	
+	public static final boolean DT_VOLTAGE_LIMIT_ENABLED = true;
+	public static final boolean DT_CURRENT_LIMIT_ENABLED = true;
+}
