bd3fc4f9b7aa2a692b7e17c8f6762768eca7cfd3
vrrajesh21 <varun.rajesh12@hotmail.com>
2018-02-10 12:33:25 -0500

    Added Intake and Outake Methods
    💯
    In Christ, Nidhi Jaison
    
    Co-Authored-By: Nidhi Jaison <njaison@users.noreply.github.com>

--- src/org/usfirst/frc/team226/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team226/robot/subsystems/Intake.java
@@ -2,24 +2,48 @@ package org.usfirst.frc.team226.robot.subsystems;
 
 import org.usfirst.frc.team226.robot.RobotMap;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Intake extends Subsystem {
-	
-	TalonSRX leftMotor = new TalonSRX(RobotMap.IN_LEFT);
-	TalonSRX rightMotor = new TalonSRX(RobotMap.IN_RIGHT);
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+    public TalonSRX left = new TalonSRX(RobotMap.IN_LEFT);
+    public TalonSRX right = new TalonSRX(RobotMap.IN_RIGHT);
+    
+    DoubleSolenoid flip = new DoubleSolenoid(0,0);
+    
+    public Intake() {
+    	
+    	right.setInverted(true);
+    	
+    	left.configVoltageCompSaturation(11.5, 0);
+    	right.configVoltageCompSaturation(11.5, 0);
+    	left.enableVoltageCompensation(true);
+    	right.enableVoltageCompensation(true);
+    	
+    }
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void moveIntake(double left, double right) {
+    	this.left.set(ControlMode.PercentOutput, left);
+    	this.right.set(ControlMode.PercentOutput, right);
+    }
+    
+    public void moveOutake(boolean move) {
+    	if (move) {
+    		left.set(ControlMode.PercentOutput, 0);
+    		right.set(ControlMode.PercentOutput, 0);
+    	}
     }
 }
 
