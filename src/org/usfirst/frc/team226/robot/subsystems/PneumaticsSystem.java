039cab0ded82c1a43309f1b4a3246248c38dc678
Alec Minchington <minchingtonak@users.noreply.github.com>
2018-02-10 12:11:00 -0500

    Drivetrain shifting code

--- src/org/usfirst/frc/team226/robot/subsystems/PneumaticsSystem.java
+++ src/org/usfirst/frc/team226/robot/subsystems/PneumaticsSystem.java
@@ -10,13 +10,46 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class PneumaticsSystem extends Subsystem {
 
 	public Compressor compressor = new Compressor(12);
-	
+
 	public PneumaticsSystem() {
 		compressor.start();
 	}
 
-    public void initDefaultCommand() {
-    }
-    
-}
+	public void initDefaultCommand() {
+	}
 
+	private DoubleSolenoid leftShifter = new DoubleSolenoid(12, 4, 5);
+	private DoubleSolenoid rightShifter = new DoubleSolenoid(12, 6, 7);
+
+	private DoubleSolenoid.Value leftVal = DoubleSolenoid.Value.kOff;
+
+	public void shiftDriveTrainLeft() {
+		if (leftVal == DoubleSolenoid.Value.kForward) {
+			leftVal = DoubleSolenoid.Value.kReverse;
+		} else {
+			leftVal = DoubleSolenoid.Value.kForward;
+		}
+		leftShifter.set(leftVal);
+	}
+
+	private DoubleSolenoid.Value rightVal = DoubleSolenoid.Value.kOff;
+
+	public void shiftDriveTrainRight() {
+		if (rightVal == DoubleSolenoid.Value.kForward) {
+			rightVal = DoubleSolenoid.Value.kReverse;
+		} else {
+			rightVal = DoubleSolenoid.Value.kForward;
+		}
+		rightShifter.set(rightVal);
+	}
+
+	public void shiftDriveTrain() {
+		shiftDriveTrainLeft();
+		shiftDriveTrainRight();
+	}
+
+	public void shiftDriveTrainNeutral() {
+		leftShifter.set(DoubleSolenoid.Value.kOff);
+		rightShifter.set(DoubleSolenoid.Value.kOff);
+	}
+}
