608dca3a686cc999b7abff88cce0c7c6e0a40886
Alec Minchington <minchingtonak@users.noreply.github.com>
2018-02-10 16:26:00 -0500

    Fix RobotMap pneumatic port naming

--- src/org/usfirst/frc/team226/robot/RobotMap.java
+++ src/org/usfirst/frc/team226/robot/RobotMap.java
@@ -22,17 +22,17 @@ public class RobotMap {
 	public static final int DT_REAR_LEFT = 13;
 	public static final int DT_REAR_RIGHT = 17;
 	
-	public static final int COMPRESSOR_A = 11;
-	public static final int COMPRESSOR_B = 12;
+	public static final int PCM_A = 11;
+	public static final int PCM_B = 12;
 	
-	public static final int COMPRESSOR_PORT = COMPRESSOR_B;
+	public static final int COMPRESSOR_PORT = PCM_B;
 	
-	public static final int LEFT_SHIFTER_PCM = COMPRESSOR_B;
-	public static final int LEFT_SHIFTER_1 = 4;
-	public static final int LEFT_SHIFTER_2 = 5;
-	public static final int RIGHT_SHIFTER_PCM = COMPRESSOR_B;
-	public static final int RIGHT_SHIFTER_1 = 6;
-	public static final int RIGHT_SHIFTER_2 = 7;
+	public static final int DT_LEFT_SHIFTER_PCM = PCM_B;
+	public static final int DT_LEFT_SHIFTER_1 = 4;
+	public static final int DT_LEFT_SHIFTER_2 = 5;
+	public static final int DT_RIGHT_SHIFTER_PCM = PCM_B;
+	public static final int DT_RIGHT_SHIFTER_1 = 6;
+	public static final int DT_RIGHT_SHIFTER_2 = 7;
 
 
 }
