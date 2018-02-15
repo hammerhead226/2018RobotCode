a5bd389df89cc23e5c332534153fbb87b0b72aa3
vrrajesh21 <varun.rajesh12@hotmail.com>
2018-02-14 19:43:37 -0500

    spacing fix in robatmap

--- src/org/usfirst/frc/team226/robot/RobotMap.java
+++ src/org/usfirst/frc/team226/robot/RobotMap.java
@@ -26,8 +26,8 @@ public class RobotMap {
 	public static final int ELEVATOR_REAR_LEFT = 0;
 	public static final int ELEVATOR_FRONT_RIGHT = 0;
 	public static final int ELEVATOR_REAR_RIGHT = 0;
-	
 	public static final int ELEVATOR_HALL_EFFECT_SENSOR = 0;
+	
 	public static final int PCM_A = 11;
 	public static final int PCM_B = 12;
 	
