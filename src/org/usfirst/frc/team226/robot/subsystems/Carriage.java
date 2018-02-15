003319886498f43bb34f7bbe8dd9de4bb48d0e97
vrrajesh21 <varun.rajesh12@hotmail.com>
2018-02-15 18:38:19 -0500

    pretty formatted

--- src/org/usfirst/frc/team226/robot/subsystems/Carriage.java
+++ src/org/usfirst/frc/team226/robot/subsystems/Carriage.java
@@ -13,27 +13,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Carriage extends Subsystem {
 
-    private TalonSRX right = new TalonSRX(RobotMap.CARRIAGE_RIGHT);
-    private TalonSRX left = new TalonSRX(RobotMap.CARRIAGE_LEFT);
-    
-    
-    
-    public Carriage() {
-    	left.setInverted(Constants.CARRIAGE_INVERT_LEFT);
-    	right.setInverted(Constants.CARRIAGE_INVERT_RIGHT);
-    }
-    
-    public void pushOut() {
-    	right.set(ControlMode.PercentOutput, Constants.CARRIAGE_OUTTAKE_SPEED);
-    	left.set(ControlMode.PercentOutput, Constants.CARRIAGE_OUTTAKE_SPEED);
-    }
-    
-    public void pullIn() {
-    	right.set(ControlMode.PercentOutput, Constants.CARRIAGE_INTAKE_SPEED);
-    	left.set(ControlMode.PercentOutput, Constants.CARRIAGE_INTAKE_SPEED);
-    }
+	private TalonSRX right = new TalonSRX(RobotMap.CARRIAGE_RIGHT);
+	private TalonSRX left = new TalonSRX(RobotMap.CARRIAGE_LEFT);
 
-    public void initDefaultCommand() {
-    }
+	public Carriage() {
+		left.setInverted(Constants.CARRIAGE_INVERT_LEFT);
+		right.setInverted(Constants.CARRIAGE_INVERT_RIGHT);
+	}
+
+	public void pushOut() {
+		right.set(ControlMode.PercentOutput, Constants.CARRIAGE_OUTTAKE_SPEED);
+		left.set(ControlMode.PercentOutput, Constants.CARRIAGE_OUTTAKE_SPEED);
+	}
+
+	public void pullIn() {
+		right.set(ControlMode.PercentOutput, Constants.CARRIAGE_INTAKE_SPEED);
+		left.set(ControlMode.PercentOutput, Constants.CARRIAGE_INTAKE_SPEED);
+	}
+
+	public void initDefaultCommand() {
+	}
 }
-
