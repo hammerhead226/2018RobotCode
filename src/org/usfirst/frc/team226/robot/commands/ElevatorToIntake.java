e635d9e85f827085342889fe80e0fcfb5e1df486
vrrajesh21 <varun.rajesh12@hotmail.com>
2018-02-16 10:21:04 -0500

    Added subsystem dependancies

--- src/org/usfirst/frc/team226/robot/commands/ElevatorToIntake.java
+++ src/org/usfirst/frc/team226/robot/commands/ElevatorToIntake.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team226.robot.commands;
 
+import org.usfirst.frc.team226.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -8,8 +10,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ElevatorToIntake extends Command {
 
     public ElevatorToIntake() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(Robot.elevator);
     }
 
     // Called just before this Command runs the first time
@@ -18,6 +19,7 @@ public class ElevatorToIntake extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.elevator.moveElevatorToIntake();
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc/team226/robot/commands/ElevatorToPower.java
+++ src/org/usfirst/frc/team226/robot/commands/ElevatorToPower.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team226.robot.commands;
 
+import org.usfirst.frc.team226.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -8,8 +10,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ElevatorToPower extends Command {
 
     public ElevatorToPower() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    	requires(Robot.elevator);
     }
 
     // Called just before this Command runs the first time
@@ -18,6 +19,7 @@ public class ElevatorToPower extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.elevator.moveElevatorToPower();
     }
 
     // Make this return true when this Command no longer needs to run execute()
