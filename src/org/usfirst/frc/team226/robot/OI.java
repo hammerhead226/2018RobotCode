7fcf720ffabc35905f49ddb42672934bca3912cd
Alec Minchington <minchingtonak@users.noreply.github.com>
2018-03-09 15:52:53 -0500

    Add competition-ready bindings

--- src/org/usfirst/frc/team226/robot/OI.java
+++ src/org/usfirst/frc/team226/robot/OI.java
@@ -7,10 +7,18 @@
 
 package org.usfirst.frc.team226.robot;
 
+import org.usfirst.frc.team226.robot.auton.grp_ExecuteSavedMacro;
+import org.usfirst.frc.team226.robot.auton.grp_ToggleAutonRecording;
+import org.usfirst.frc.team226.robot.commands.ArmSetpointGround;
+import org.usfirst.frc.team226.robot.commands.ArmSetpointHigh;
+import org.usfirst.frc.team226.robot.commands.ArmSetpointSwitch;
 import org.usfirst.frc.team226.robot.commands.HardZeroArmEncoder;
-import org.usfirst.frc.team226.robot.commands.ShiftIntakeLeft;
-import org.usfirst.frc.team226.robot.commands.ShiftIntakeRight;
+import org.usfirst.frc.team226.robot.commands.ShiftDriveTrainHighGear;
+import org.usfirst.frc.team226.robot.commands.ShiftDriveTrainLowGear;
+import org.usfirst.frc.team226.robot.commands.ShiftIntake;
 import org.usfirst.frc.team226.robot.commands.Shoot;
+import org.usfirst.frc.team226.robot.commands.auto_Intake;
+import org.usfirst.frc.team226.robot.commands.grp_ShootOuttake;
 
 import util.Controller;
 
@@ -22,11 +30,27 @@ public class OI {
 
 	public Controller driver = new Controller(0);
 	public Controller manip = new Controller(1);
-	
+
 	public OI() {
-		manip.getLBButton().whileHeld(new HardZeroArmEncoder());
-		driver.getBButton().whenPressed(new Shoot());
-		driver.getAButton().whenPressed(new ShiftIntakeRight());
-		driver.getBButton().whenPressed(new ShiftIntakeLeft());		
+		// Drivetrain
+		driver.getLSButton().whenPressed(new ShiftDriveTrainHighGear());
+		driver.getRSButton().whenPressed(new ShiftDriveTrainLowGear());
+
+		// Intake
+		manip.getRBButton().whenPressed(new ShiftIntake());
+		manip.getRSButton().whenPressed(new auto_Intake());
+		manip.getBButton().whenPressed(new grp_ShootOuttake());
+		manip.getSTARTButton().whenPressed(new Shoot());
+
+		// Arm
+		manip.getSELECTButton().whenPressed(new HardZeroArmEncoder());
+		manip.getYButton().whenPressed(new ArmSetpointHigh());
+		manip.getXButton().whenPressed(new ArmSetpointSwitch());
+		manip.getAButton().whenPressed(new ArmSetpointGround());
+
+		// Auton recording controls
+		driver.getSTARTButton().whenPressed(new grp_ToggleAutonRecording());
+		driver.getSELECTButton().whenPressed(new grp_ExecuteSavedMacro());
 	}
+
 }
