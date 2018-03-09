0bd5426f3dfd3f586f268a580da868ff7ac4e947
Alec Minchington <minchingtonak@users.noreply.github.com>
2018-03-09 14:05:59 -0500

    Refactor some port names, final pre-indiana port numbers

--- src/org/usfirst/frc/team226/robot/RobotMap.java
+++ src/org/usfirst/frc/team226/robot/RobotMap.java
@@ -22,28 +22,31 @@ public class RobotMap {
 	public static final int DT_REAR_LEFT = 13;
 	public static final int DT_REAR_RIGHT = 17;
 	
+	public static final int INTAKE_LEFT = 14;
+	public static final int INTAKE_RIGHT = 18;
+	public static final int ARM_RIGHT = 21;
+	public static final int ARM_LEFT = 19;
+	
 	public static final int PCM_A = 11;
 	public static final int PCM_B = 12;
 	
 	public static final int COMPRESSOR_PORT = PCM_B;
 	
 	public static final int DT_LEFT_SHIFTER_PCM = PCM_B;
-	public static final int DT_LEFT_SHIFTER_1 = 4;
-	public static final int DT_LEFT_SHIFTER_2 = 5;
+	public static final int DT_LEFT_SHIFTER_1 = 0;
+	public static final int DT_LEFT_SHIFTER_2 = 1;
 	public static final int DT_RIGHT_SHIFTER_PCM = PCM_B;
-	public static final int DT_RIGHT_SHIFTER_1 = 6;
-	public static final int DT_RIGHT_SHIFTER_2 = 7;
-	public static final int INTAKE_LEFT_ID = 0;
-	public static final int INTAKE_RIGHT_ID = 1;
-	public static final int ARM_LEFT_ID = 21;
-	public static final int ARM_RIGHT_ID = 19;
+	public static final int DT_RIGHT_SHIFTER_1 = 2;
+	public static final int DT_RIGHT_SHIFTER_2 = 3;
+	
 	public static final int ARM_PCM = PCM_A;
-	public static final int SHOOTER_1 = 0;
-	public static final int SHOOTER_2 = 1;
-	public static final int INTAKE_LEFT_1 = 2;
-	public static final int INTAKE_LEFT_2 = 3;
-	public static final int INTAKE_RIGHT_1 = 0;
-	public static final int INTAKE_RIGHT_2 = 1;
-	public static final int INTAKE_PCM = PCM_B;
+	public static final int SHOOTER_PISTON_1 = 4;
+	public static final int SHOOTER_PISTON_2 = 5;
+	public static final int INTAKE_PCM = PCM_A;
+	public static final int INTAKE_LEFT_SHIFTER_1 = 2;
+	public static final int INTAKE_LEFT_SHIFTER_2 = 3;
+	public static final int INTAKE_RIGHT_SHIFTER_1 = 0;
+	public static final int INTAKE_RIGHT_SHIFTER_2 = 1;
+	
 			
 }
