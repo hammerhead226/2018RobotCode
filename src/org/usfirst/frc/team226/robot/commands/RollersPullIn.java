3eb45bd9c457fa1f87f6dfd8310128eca6b3dea0
vrrajesh21 <varun.rajesh12@hotmail.com>
2018-03-13 19:55:07 -0400

    added 0 speed method at end of pull in

--- src/org/usfirst/frc/team226/robot/commands/RollersPullIn.java
+++ src/org/usfirst/frc/team226/robot/commands/RollersPullIn.java
@@ -30,6 +30,7 @@ public class RollersPullIn extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.intake.rollersPullIn(0);
     }
 
     // Called when another command which requires one or more of the same
